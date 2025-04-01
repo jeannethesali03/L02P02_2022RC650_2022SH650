7a758b3d0905df92482264a2af0c3d4f7bc634c3
wols616 <wilber.rivas1@catolica.edu.sv>
2025-04-01 16:56:14 -0600

    Add project files.

---
+++ L02P02_2022RC650_2022SH650/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using L02P02_2022RC650_2022SH650.Models;
+
+namespace L02P02_2022RC650_2022SH650.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
